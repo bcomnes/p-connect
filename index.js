5b24d0482432eb0ba7b21ffc8f8c21fb970a0752
Bret Comnes <bcomnes@gmail.com>
2019-07-29 18:15:42 +0200

    fix more naming things

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ exports.pMiddleware = function pMiddleware (middleware) {
   }
 }
 
-exports.route = function pRoute (route) {
+exports.route = function route (route) {
   return (req, res, next) => {
     route(req, res).then(() => done(null)).catch(next)
   }
 }
 
 // For http-hash-router style routes
-exports.hashRoute = function pHashRoute (hashRoute) {
+exports.hashRoute = function route (hashRoute) {
   return (req, res, opts, done) => {
     hashRoute(req, res, opts).then(() => done(null)).catch(done)
   }
